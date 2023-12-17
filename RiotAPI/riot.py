15b20620acfce8bc356279bde30a556eb020bcbf
AidanWalters <aidanpwalters@gmail.com>
2023-12-17 03:32:59 -0500

    Added Pie chart

--- RiotAPI/riot.py
+++ RiotAPI/riot.py
@@ -2,6 +2,7 @@
 
 import requests
 import os
+import matplotlib.pyplot as plt
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -113,21 +114,9 @@ def getFavoriteChampion(name) -> str:
     #From the masteryData grab the champions ID to be used to find that champion's info
     championId = (masteryData[0]['championId'])
 
-    #URL for champion data file
-    url = "https://ddragon.leagueoflegends.com/cdn/13.24.1/data/en_US/champion.json"
-    response = requests.get(url)
-    championData = response.json() #Parse content as JSON
-
-
-    #Loop through json file and check each champion for a key match to the id of the user's most used champion
-    for i in range(len(championData["data"])):
-        champion = list(championData["data"].keys())[i]
-        if championData["data"][champion].get("key") == str(championId):
-            favChamp = champion
-            #print(f"found {favChamp}")
-            break
     
-    return(f"{champion}")
+    
+    return(f"{getChampionName(championId)}")
 
 
 
@@ -141,6 +130,55 @@ def getPUUID(name):
     return PUUID
 
 
+def championPie(name, amount):
+    #Get PUUID to gather the user's champion mastery info
+    PUUID = getPUUID(name)
+    account_url = (f"https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-puuid/{PUUID}?api_key={API_KEY}")
+    masteryResponse = requests.get(account_url) #Request GET
+    
+    if masteryResponse.status_code != 200:
+        print(masteryResponse.status_code)
+        return None
 
+    masteryData = masteryResponse.json()
+
+    if len(masteryData) < amount:
+        return("Try a smaller amount of champions!")
+
+    #Only get the amount of masteries specified
+    #Use a mastery name array to hold the arrays of the corresponding masteries
+    masteryData = masteryData[0:amount]
+    masteryNames = [0] * amount
+    masteryPoints = [0] * amount
+    
+    #populate array for labels/champ names
+    for i in range(amount):
+        championId = masteryData[i]['championId']
+        masteryNames[i] = getChampionName(championId)
+        masteryPoints[i] = masteryData[i]['championPoints']
+    
+    fig, ax = plt.subplots()
+    ax.pie(masteryPoints, labels=masteryNames, autopct='%1.1f%%', radius=1.2)
+    ax.set(aspect="equal")
+    ax.set_title(f"Champion Pie for {name} Top {amount} Masteries", y=1.075)
+    plt.show()
+
+    return("Success!")
+
+    
+
+def getChampionName(championId) -> str:
+    #URL for champion data file
+    url = "https://ddragon.leagueoflegends.com/cdn/13.24.1/data/en_US/champion.json"
+    response = requests.get(url)
+    championData = response.json() #Parse content as JSON
+
+    #Loop through json file and check each champion for a key match to the id
+    for i in range(len(championData["data"])):
+        champion = list(championData["data"].keys())[i]
+        if championData["data"][champion].get("key") == str(championId):
+            foundChamp = champion
+            break
+    return foundChamp
 
 
